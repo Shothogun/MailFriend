329e5e1eb31d6e744b1b3933d632ed71f124cc1f
Otho TK <otho.tk@hotmail.com>
2022-04-14 09:38:34 -0300

    :art: Classifier fix

--- classifier.py
+++ classifier.py
@@ -1,77 +1,99 @@
 from datetime import date, datetime, timedelta
 import os
 
+
+def deadline_compute(ticketDate: datetime):
+    deadline = ticketDate
+
+    total_hours = 8
+
+    while total_hours > 0:
+        deadline += timedelta(hours=1)
+        if deadline.hour == 18:
+            deadline = deadline + timedelta(days=1)
+            deadline = datetime(deadline.year, deadline.month, deadline.day, 8,
+                                deadline.minute, deadline.second)
+        total_hours -= 1
+
+    return deadline
+
+
 def secondsToHours(seconds: int):
-  return seconds//3600
+    return seconds // 3600
+
 
 def commercialBeginDate(date: datetime):
-  if date.hour < 8:
-    return datetime(date.year,date.month, date.day, 8)
-  elif date.hour >= 18:
-    date = date + timedelta(days=1)
-    return datetime(date.year,date.month, date.day, 8)
-  else:
-    return date
+    if date.hour < 8:
+        return datetime(date.year, date.month, date.day, 8)
+    elif date.hour >= 18:
+        date = date + timedelta(days=1)
+        return datetime(date.year, date.month, date.day, 8)
+    else:
+        return date
+
 
 def commercialTimePassed(previousDate: datetime, now: datetime):
-  total = 0
-  current_date = previousDate
-  # TODO raise error with day after today
+    total = 0
+    current_date = previousDate
+    # TODO raise error with day after today
 
-  # Count previous days hours
-  while current_date.day != now.day:
-    day_deadline = datetime(current_date.year, current_date.month,
-                            current_date.day, 18, 0 ,0)
-    total += secondsToHours((day_deadline - current_date).seconds)
+    # Count previous days hours
+    while current_date.day != now.day:
+        day_deadline = datetime(current_date.year, current_date.month,
+                                current_date.day, 18, 0, 0)
+        total += secondsToHours((day_deadline - current_date).seconds)
 
-    current_date += timedelta(days=1)
-    current_date = datetime(current_date.year, current_date.month,
-                            current_date.day, 8, 0 ,0)
+        current_date += timedelta(days=1)
+        current_date = datetime(current_date.year, current_date.month,
+                                current_date.day, 8, 0, 0)
 
-  total += secondsToHours((now - current_date).seconds)
+    total += secondsToHours((now - current_date).seconds)
 
-  return total
+    return total
+
 
 def classifyTicket(call):
-  ticketDate = datetime.strptime(call['Data_do_Registro'], "%d/%m/%Y %H:%M:%S")
-  ticketDate = commercialBeginDate(ticketDate)
+    ticketDate = datetime.strptime(call['Data_do_Registro'],
+                                   "%d/%m/%Y %H:%M:%S")
+    ticketDate = commercialBeginDate(ticketDate)
 
-  timeLeft = 8 - commercialTimePassed(ticketDate, datetime.now())
+    deadline = deadline_compute(ticketDate)
 
-  if timeLeft <= 0:
-    return (call, 'V')
-  elif 0 < timeLeft <= 2:
-    return (call, 'B')
-  else:
-    return (call, 'A')
-  
+    timeLeft = commercialTimePassed(datetime.now(), deadline)
+
+
+    if((deadline -  datetime.now()).days < 0):
+      timeLeft = 0
+      
+    if timeLeft <= 0:
+        return (call, 'V')
+    elif 0 < timeLeft <= 2:
+        return (call, 'B')
+    else:
+        return (call, 'A')
+
 
 def updateRemainingTime(call_tuple):
-  call, callClass = call_tuple
+    call, callClass = call_tuple
 
-  if callClass == 'V':
-    call['Tempo_Restante'] = "Vencido!"
-    return (call,callClass)
+    if callClass == 'V':
+        call['Tempo_Restante'] = "Vencido!"
+        return (call, callClass)
 
-  ticketDate = datetime.strptime(call['Data_do_Registro'], "%d/%m/%Y %H:%M:%S")
-  ticketDate = commercialBeginDate(ticketDate)
+    ticketDate = datetime.strptime(call['Data_do_Registro'],
+                                   "%d/%m/%Y %H:%M:%S")
+    ticketDate = commercialBeginDate(ticketDate)
 
-  total_hours = 8
+    deadline = deadline_compute(ticketDate)
 
-  deadline = ticketDate
+    timeLeft = commercialTimePassed(datetime.now(), deadline)
+    daysLeft = timeLeft // 8
+    hoursLeft = timeLeft % 8
 
-  while total_hours > 0:
-    deadline += timedelta(hours=1)
-    if deadline.hour == 18:
-      deadline = deadline + timedelta(days=1)
-      deadline = datetime(deadline.year, deadline.month,
-                          deadline.day, 8, deadline.minute ,deadline.second)
-    total_hours -= 1
+    if((deadline -  datetime.now()).days < 0):
+      daysLeft = 0
+      hoursLeft = 0
 
-  timeLeft = commercialTimePassed(datetime.now(), deadline)
-  daysLeft = timeLeft//8
-  hoursLeft = timeLeft%8
+    call['Tempo_Restante'] = "{} dia(s) e {}h".format(daysLeft, hoursLeft)
 
-  call['Tempo_Restante'] = "{} dia(s) e {}h".format(daysLeft,hoursLeft)
-
-  return (call,callClass)
+    return (call, callClass)
